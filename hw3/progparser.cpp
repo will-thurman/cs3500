e9234d8db4ad90de0194a05f46f096a57b44c1e2
William Thurman <William Thurman>
2017-03-10 18:07:31 -0600

    Testing int parser

--- hw3/progparser.cpp
+++ hw3/progparser.cpp
@@ -11,50 +11,46 @@ class ProgotronParser
     char m_next;
     
 	  ProgotronParser()
-		{
-			get_token(); //Initialize the first token
-		}
+    {
+      m_next = cin.peek();
+    }
 		
 	  void get_token();
     void skip_whitespace();
 		
-		bool parse_int(); 
-		bool parse_dec();
-		bool parse_str();
-		bool parse_id();
+    bool parse_int(); 
+    bool parse_dec();
+    bool parse_str();
+    bool parse_id();
+    
+    bool parse_rel();    // Relation :=   < | > | = | #  
+    bool parse_add_op(); // AddOperator :=   + | - | OR | &
+    bool parse_mul_op(); // MulOperator :=   * | / | AND
 		
-		bool parse_rel();    // Relation :=   < | > | = | #  
-		bool parse_add_op(); // AddOperator :=   + | - | OR | &
-		bool parse_mul_op(); // MulOperator :=   * | / | AND
-		
-		bool parse_exp();    // Expression := SimpleExpression [ Relation SimpleExpression ]
-		bool parse_sim_exp();// SimpleExpression := Term { AddOperator Term }
-		bool parse_term();   // Term := Factor { MulOperator Factor }
-		bool parse_fact();   // Factor :=  integer | decimal | string | identifier | ( Expression ) | ~ Factor
-		
-		bool parse_assign(); // Assignment := identifier := Expression !
-		bool parse_print();  // PrintStatement := PRINT ( Expression )!
-		bool parse_ret();    // RetStatement := RET identifier !
-		bool parse_if();     // IfStatement := IF ( Expression ) StatementSequence [ ELSE StatementSequence ] FI
-		bool parse_loop();   // LoopStatement := LOOP ( Expression ) StatementSequence POOL
-		
+    bool parse_exp();    // Expression := SimpleExpression [ Relation SimpleExpression ]
+    bool parse_sim_exp();// SimpleExpression := Term { AddOperator Term }
+    bool parse_term();   // Term := Factor { MulOperator Factor }
+    bool parse_fact();   // Factor :=  integer | decimal | string | identifier | ( Expression ) | ~ Factor
+
+    bool parse_assign(); // Assignment := identifier := Expression !
+    bool parse_print();  // PrintStatement := PRINT ( Expression )!
+    bool parse_ret();    // RetStatement := RET identifier !
+    bool parse_if();     // IfStatement := IF ( Expression ) StatementSequence [ ELSE StatementSequence ] FI
+    bool parse_loop();   // LoopStatement := LOOP ( Expression ) StatementSequence POOL
+
 		bool parse_statement();     // Statement := Assignment | PrintStatement | RetStatement | IfStatement | LoopStatement
-		bool parse_statement_seq(); // StatementSequence := Statement { Statement }
-		
-		bool parse_param_seq(); // ParamSequence :=  identifier  { , identifier }
-		bool parse_func_dec();  // FunctionDeclaration := FUNC identifier ( [ ParamSequence ] ) BEGIN StatementSequence END.
-		
-		bool parse_func_seq();  // FunctionSequence := FunctionDeclaration { FunctionDeclaration }
+	  bool parse_statement_seq(); // StatementSequence := Statement { Statement }
+
+    bool parse_param_seq(); // ParamSequence :=  identifier  { , identifier }
+    bool parse_func_dec();  // FunctionDeclaration := FUNC identifier ( [ ParamSequence ] ) BEGIN StatementSequence END.
+
+    bool parse_func_seq();  // FunctionSequence := FunctionDeclaration { FunctionDeclaration }
 };
 
 int main()
 {
   ProgotronParser parse;
-  while(!cin.eof())
-  {
-	  parse.get_token();
-    // parse.skip_whitespace();
-  }
+  parse.parse_int();
   cout << endl;
   return 0;
 }
@@ -62,20 +58,25 @@ int main()
 bool ProgotronParser::parse_int()
 {
   
-  if(m_token == '+' || m_token == '-')
+  if(m_next == '+' || m_next == '-')
   {
     get_token();
   }
-  else if(isdigit(m_token))
+  
+  if(isdigit(m_next))
   {
+    while(isdigit(m_next))
+      get_token();
     
+    if(isspace(m_next))
+      return true;
+    skip_whitespace();
   }
   return false;
 }
 
 void ProgotronParser::get_token()
 {
-  
   m_token = cin.get();
   m_next = cin.peek();
   cout << m_token;
